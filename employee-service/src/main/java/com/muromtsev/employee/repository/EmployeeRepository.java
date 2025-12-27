aee9a92fbfbe28b96c0248601df33a65d03835ca
muromtsev <t4k.org@mail.ru>
2025-12-28 00:51:33 +0300

    added jpa repository

---
+++ employee-service/src/main/java/com/muromtsev/employee/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.muromtsev.employee.repository;
+
+import com.muromtsev.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findByOrganizationId(String organizationId);
+    Employee findByOrganizationIdAndEmployeeId(String organizationId, String employeeId);
+
+}
